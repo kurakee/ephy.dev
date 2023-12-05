ae0c8924d6623134b29b0f63a8f606e524236b6d
kurake <ephyrar@gmail.com>
2023-12-05 22:43:50 +0900

    アイコン表示してみるなど

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -3,30 +3,24 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1>ephy.dev</h1>
+    <main>
+      <p>About page</p>
       <img
         src="https://ephy.assets.newt.so/v1/f43f79d6-316e-4ba4-b9db-c40a6ed0bb0e/icon-2045450479.png"
         height={128}
         width={128}
         alt="icon"
       />
-      <p>Web Developer / Software Engineer</p>
-      <p>Nagano / Japan</p>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/articles">Articles</a></li>
-      </ul>
-    </>
+    </main>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Newt・Qwik Cityブログ",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "NewtとQwik Cityを利用したブログです",
     },
   ],
 };
