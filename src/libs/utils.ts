2140e482b5460db01d42c6bc45632910e7d507e2
kurake <ephyrar@gmail.com>
2023-12-08 03:44:11 +0900

    日付フォーマットにタイムゾーン入れ忘れていた

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,5 +2,10 @@
  * 日付文字列をフォーマット
  */
 export const formatDate = (date: string): string => {
-  return new Date(date).toLocaleDateString("ja-jp", { year: "numeric", month: "numeric", day: "numeric" });
+  return new Date(date).toLocaleDateString("ja-jp", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    timeZone: "Asia/Tokyo",
+  });
 };
