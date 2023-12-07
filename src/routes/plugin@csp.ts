072c4c3ff9509db542d1731e263f1cd3fac483c4
kurake <ephyrar@gmail.com>
2023-12-07 18:34:51 +0900

    CSP ytimg追加

--- src/routes/plugin@csp.ts
+++ src/routes/plugin@csp.ts
@@ -7,8 +7,9 @@ export const onRequest: RequestHandler = (event) => {
   event.sharedMap.set("@nonce", nonce);
   const csp = [
     `default-src 'self' 'unsafe-inline' *.newt.so *.cloudflareaccess.com`,
+    `connect-src 'self' data: blob:`,
     `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' 'strict-dynamic'`,
-    `frame-src 'self' 'nonce-${nonce}' youtube.com *.youtube.com *.google.com`,
+    `frame-src 'self' 'nonce-${nonce}' youtube.com *.youtube.com *.ytimg.com *.google.com`,
   ];
 
   event.headers.set("Content-Security-Policy", csp.join("; "));
