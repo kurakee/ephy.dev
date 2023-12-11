04dd7100eee05bcf944e13fd85cb75099c85ee0a
kurake <ephyrar@gmail.com>
2023-12-12 02:06:45 +0900

    storage.ephy.devへのCSP対応漏れ

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -8,11 +8,7 @@
  * You can also use this file to add more functionality that runs in the service worker.
  */
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
-
 setupServiceWorker();
-
 addEventListener("install", () => self.skipWaiting());
-
 addEventListener("activate", () => self.clients.claim());
-
 declare const self: ServiceWorkerGlobalScope;
