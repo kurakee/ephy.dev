dbafdee5ef566e24d528faec4d9357fb1941b42f
kurake <ephyrar@gmail.com>
2024-01-06 00:16:32 +0900

    GoogleAnalyticsが反応してくれないので新しいタグ設定をしてみる

--- src/root.tsx
+++ src/root.tsx
@@ -18,7 +18,7 @@ export default component$(() => {
       <head>
         <meta charSet="UTF-8" />
         <QwikPartytown forward={["dataLayer.push"]} />
-        <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-ZM24XR3CYH" />
+        <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-5F5FLEVQJ2" />
         <meta name="theme-color" content="#b1d5ff" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
