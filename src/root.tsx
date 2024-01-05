f7e30d51533419595ba2ea738ced7fe9ad3f24b2
kurake <ephyrar@gmail.com>
2024-01-05 23:41:50 +0900

    gtm 動作確認3

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { component$, useServerData } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 import { RouterHead } from "~/components/router-head/router-head";
 
@@ -6,8 +6,7 @@ import "~/styles/global.css";
 import { QwikPartytown } from "./components/partytown/partytown";
 
 export default component$(() => {
-  const GA_MEASUREMENT_ID = "G-ZM24XR3CYH";
-  const nonce = useServerData<string | undefined>("nonce");
+  // const nonce = useServerData<string | undefined>("nonce");
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
@@ -19,12 +18,7 @@ export default component$(() => {
       <head>
         <meta charSet="UTF-8" />
         <QwikPartytown forward={["dataLayer.push"]} />
-        <script
-          async
-          type="text/partytown"
-          nonce={nonce}
-          dangerouslySetInnerHTML={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_MEASUREMENT_ID}');`}
-        />
+        <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-ZM24XR3CYH" />
         <meta name="theme-color" content="#b1d5ff" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -34,7 +28,7 @@ export default component$(() => {
       </head>
       <body lang="ja" class="text-secondary-500 min-h-screen bg-white antialiased">
         <RouterOutlet />
-        <ServiceWorkerRegister nonce={nonce} />
+        <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
   );
--- src/routes/plugin@csp.ts
+++ src/routes/plugin@csp.ts
@@ -3,9 +3,9 @@ import { isDev } from "@builder.io/qwik/build";
 
 export const onRequest: RequestHandler = (event) => {
   if (isDev) return;
-  const str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const nonce = Array.from({ length: 16 }, () => str[Math.floor(Math.random() * str.length)]).join("");
-  event.sharedMap.set("@nonce", nonce);
+  // const str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  // const nonce = Array.from({ length: 16 }, () => str[Math.floor(Math.random() * str.length)]).join("");
+  // event.sharedMap.set("@nonce", nonce);
   const csp = [
     ["default-src", "'self'", "'unsafe-inline'"],
     ["connect-src", "'self'", "data:", "blob:"],
@@ -13,12 +13,12 @@ export const onRequest: RequestHandler = (event) => {
       "script-src",
       "'self'",
       "https:",
-      `'nonce-${nonce}'`,
+      "'unsafe-inline'",
       "strict-dynamic",
       "cdn.iframe.ly",
       "www.googletagmanager.com",
     ],
-    ["frame-src", "'self'", `'nonce-${nonce}'`, "*.youtube.com", "*.google.com", "cdn.iframe.ly"],
+    ["frame-src", "'self'", "*.youtube.com", "*.google.com", "cdn.iframe.ly"],
     [
       "img-src",
       "'self'",
